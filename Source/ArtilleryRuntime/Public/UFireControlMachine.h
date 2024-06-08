0d5f144002343579f2b7dfc9a9dd94687e3ad096
JKurzer <jake.kurzer@gmail.com>
2024-06-07 20:21:46 -0700

    Structure the fire control machine to push patterns to the runner thread.

--- Source/ArtilleryRuntime/Public/UFireControlMachine.h
+++ Source/ArtilleryRuntime/Public/UFireControlMachine.h
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-/*
+
 #pragma once
 
 #include "CoreMinimal.h"
@@ -9,30 +9,50 @@
 #include "GameplayEffect.h"
 #include "FArtilleryGun.h"
 #include "Abilities/GameplayAbility.h"
-
+#include "Containers/CircularQueue.h"
+#include <bitset>
 #include "UAbilityFireControl.generated.h"
 
 //dynamic constructed statemachine for matching patterns in action records to triggering abilities.
 
-class ARTILLERYRUNTIME_API UFireControlMachine : public 
+struct FActionBitMask
+{
+	std::bitset<14> buttons;
+	std::bitset<6> events;
+};
+
+typedef uint64_t FGunKey;
+
+class FActionPattern 
+{
+	virtual bool runPattern(FActionBitMask ToSeek) = 0;
+};
+
+
+//Generally, a firecontrolmachine represents a specific gun or set of guns
+//for a specific actor. this, combined with the ACS, allows you to fully 
+//integrate artillery with your existing GAS designs.
+class ARTILLERYRUNTIME_API UFireControlMachine : public UActorComponent
 
 {
 public:	
 		UPROPERTY(EditAnywhere, BlueprintReadOnly)
-		TObjectPtr<gun collection goes here> my guns;
+		TMap<FGunKey, FArtilleryGun> MyManagedGuns;
+		UAttributeSet MyAttributes; // might want to defactor this to an ECS, but I actually quite like it here.
+		UAbilitySystemComponent* SystemComponentToBind;
+		bool pushPatternToRunner(FActionPattern ToBind, FActionBitMask ToSeek, FGunKey ToFire)
+		{
+			return false;
+		};
 
-		function to load collection from data or config.
-
-			function to add
-			function to remove
-
-		//stop execution BP node needed here
-		UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Latency Hiding")
-		int DallyFramesToOmit = 0;
-		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Latency Hiding")
-		bool RerunDueToReconcile = false;
-
-		fire( only fire if in myguns)
+		bool FireGun(GunKey GunID, bool InputAlreadyUsedOnce)
+		{
+			return false;
+		};
+		
+		void BeginPlay() override {
+			Super();
+			//likely want to manage system component bind here by checking for actor parent.
+			UActorComponent::GetOwner();
+		};
 };
-
-*/
