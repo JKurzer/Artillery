36028e1475cf32cd4ceb6541730484194fb9f7e8
JKurzer <jake.kurzer@gmail.com>
2024-06-14 01:02:42 -0700

    Removes the overridden handling of EndAbility
    This just leaves the added behavior we use for chaining, but doesn't remove any of the incredibly gross stuff the base class does.
    I REALLY dislike this, as it touches world, but it is simplest. and the best code is no code.

--- Source/ArtilleryRuntime/Private/UArtilleryAbilityMinimum.cpp
+++ Source/ArtilleryRuntime/Private/UArtilleryAbilityMinimum.cpp
@@ -91,89 +91,9 @@ void UArtilleryPerActorAbilityMinimum::PreActivate(const FGameplayAbilitySpecHan
 
 void UArtilleryPerActorAbilityMinimum::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
-	if (IsEndAbilityValid(Handle, ActorInfo))
-	{
-		if (ScopeLockCount > 0)
-		{
-			UE_LOG(LogTemp, Verbose, TEXT("Attempting to end Ability %s but ScopeLockCount was greater than 0, adding end to the WaitingToExecute Array"), *GetName());
-			WaitingToExecute.Add(FPostLockDelegate::CreateUObject(this, &UArtilleryPerActorAbilityMinimum::EndAbility, Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled));
-			return;
-		}
-        
-        if (GetInstancingPolicy() != EGameplayAbilityInstancingPolicy::NonInstanced)
-        {
-            bIsAbilityEnding = true;
-        }
-
-		// Give blueprint a chance to react
-		K2_OnEndAbility(bWasCancelled);
-
-		// Protect against blueprint causing us to EndAbility already
-		if (bIsActive == false && GetInstancingPolicy() != EGameplayAbilityInstancingPolicy::NonInstanced)
-		{
-			return;
-		}
-
-
-
-		if (GetInstancingPolicy() != EGameplayAbilityInstancingPolicy::NonInstanced)
-		{
-			bIsActive = false;
-			bIsAbilityEnding = false;
-		}
-
-		// Tell all our tasks that we are finished and they should cleanup
-		for (int32 TaskIdx = ActiveTasks.Num() - 1; TaskIdx >= 0 && ActiveTasks.Num() > 0; --TaskIdx)
-		{
-			UGameplayTask* Task = ActiveTasks[TaskIdx];
-			if (Task)
-			{
-				Task->TaskOwnerEnded();
-			}
-		}
-		ActiveTasks.Reset();	// Empty the array but don't resize memory, since this object is probably going to be destroyed very soon anyways.
-
-		if (UAbilitySystemComponent* const AbilitySystemComponent = ActorInfo->AbilitySystemComponent.Get())
-		{
-			// Remove tags we added to owner
-			AbilitySystemComponent->RemoveLooseGameplayTags(ActivationOwnedTags);
-			
-			// Remove tracked GameplayCues that we added
-			for (FGameplayTag& GameplayCueTag : TrackedGameplayCues)
-			{
-				AbilitySystemComponent->RemoveGameplayCue(GameplayCueTag);
-			}
-			TrackedGameplayCues.Empty();
-
-			if (CanBeCanceled())
-			{
-				// If we're still cancelable, cancel it now
-				AbilitySystemComponent->HandleChangeAbilityCanBeCanceled(AbilityTags, this, false);
-			}
-			
-			if (IsBlockingOtherAbilities())
-			{
-				// If we're still blocking other abilities, cancel now
-				AbilitySystemComponent->ApplyAbilityBlockAndCancelTags(AbilityTags, this, false, BlockAbilitiesWithTag, false, CancelAbilitiesWithTag);
-			}
-
-			AbilitySystemComponent->ClearAbilityReplicatedDataCache(Handle, CurrentActivationInfo);
-
-			// Tell owning AbilitySystemComponent that we ended so it can do stuff (including MarkPendingKill us)
-			AbilitySystemComponent->NotifyAbilityEnded(Handle, this, bWasCancelled);
-		}
-
-		if (IsInstantiated())
-		{
-			CurrentEventData = FGameplayEventData{};
-		}
-
-		// WE DO NOT UNBIND as our single instance per actor is held by an FArtillery gun under the control of
-		// both the ability system component and the Artillery fire control.
-	
+		Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 		FArtilleryStates HowDidItGo = bWasCancelled ? FArtilleryStates::Fired : FArtilleryStates::Canceled ;
 		GunBinder.Execute(HowDidItGo, AvailableDallyFrames, ActorInfo, ActivationInfo);
-	}
 }
 
 bool UArtilleryPerActorAbilityMinimum::CommitAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, OUT FGameplayTagContainer* OptionalRelevantTags)
